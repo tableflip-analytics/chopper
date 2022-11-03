90c899d8a12b24c3e010b4a4e891fcffa023739f
Joshua Matfess <josh@table-flip.net>
2022-11-03 00:57:58 -0400

    big refactor

--- chopper.py
+++ chopper.py
@@ -1,165 +1,292 @@
 import argparse
 import csv
+import glob
 import os
+import random
 import re
+import shutil
+import tempfile
 
-from itertools import repeat
-from multiprocessing import Pool
-from random import shuffle
+from itertools import cycle
+from typing import Any
+
+TMP_DIR = tempfile.TemporaryDirectory()
 
 
-def parse_args() -> dict:
+def parse_args() -> dict[str, Any]:
     parser = argparse.ArgumentParser(
-        description="Get into the CHOPPER so you can Create Hundreds Of Petite Pieces Easily...Really."
+        description="Get into the CHOPPER so you can Create Hoardes Of Punier Pieces Evading RAM."
     )
     parser.add_argument(
-        "input_file",
+        "input_path",
         type=str,
-        help="Absolute or relative path of file to chop.",
+        help="Input file or directory. If a directory is specified, CHOPPER will treat all files in the directory (including subdirectories) as a single file for splitting and shuffling purposes.",
     )
     parser.add_argument(
         "output_directory",
         type=str,
-        help="Absolute or relative path of folder to output to. An empty/nonexistent folder is highly recommended, as CHOPPER may overwrite files.",
+        help="An empty/nonexistent folder is highly recommended, as CHOPPER may overwrite files.",
     )
     parser.add_argument("-e", "--encoding", type=str, required=False, default=None)
-    parser.add_argument(
-        "-p",
-        "--prefix",
-        type=str,
-        help="String prepended to each output file",
-        required=False,
-        default="",
-    )
-    parser.add_argument("-c", "--columns", type=str, help="Comma separated list of field names to split by.", required=False, default="")
-    parser.add_argument("-r", "--rows", help="Maximum number of rows per file.", type=int, required=False, default=0)
-    parser.add_argument(
-        "-s",
-        "--shuffle",
-        action="store_true",
-        help="WARNING: In the worst case scenario (when only splitting by row count and not any column), setting this flag requires loading the entire input file to memory. When the columns argument is used, CHOPPER will perform shuffles after splitting by those columns to keep memory use as low as possible.",
-    )
     parser.add_argument(
         "-d",
         "--delimiter",
         help="Delimiter to use when parsing the input file. Default ','.",
         type=str,
         required=False,
-        default=","
+        default=",",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        type=str,
+        help="String prepended to each output file",
+        required=False,
+    )
+    actions = parser.add_argument_group()
+    actions.add_argument(
+        "-s",
+        "--shuffles",
+        help="Perform N shuffles. Outputs one set of chopped files per shuffle. WARNING: In the worst case scenario (when only splitting by row count and not any column), setting this flag requires loading the entire input file to memory. When the columns argument is used, CHOPPER will perform shuffles after splitting by those columns to keep memory use as low as possible.",
+        type=int,
+        required=False,
+        default=0,
+    )
+    actions.add_argument(
+        "-c",
+        "--columns",
+        type=str,
+        help="Comma separated list of field names to split by.",
+        required=False,
+        default="",
+    )
+    row_group = actions.add_mutually_exclusive_group()
+    row_group.add_argument(
+        "-r",
+        "--rows",
+        help="Maximum number of rows per file.",
+        type=int,
+        required=False,
+        default=0,
+    )
+    row_group.add_argument(
+        "-q",
+        "--equal",
+        help="Split file into X files of approximately equal row counts. When used in conjunction with the --columns argument, this will split each subgroup into X files.",
+        type=int,
+        required=False,
+        default=0,
     )
 
-    return vars(parser.parse_args())
+    args = vars(parser.parse_args())
+
+    actions = ["columns", "rows", "equal", "shuffles"]
+    if not args.keys() & actions:
+        parser.error(
+            "You must specify at least one action argument (columns, rows, equal, shuffles)"
+        )
+
+    return args
 
 
-def get_csv_dialect(file: str, encoding) -> csv.Dialect:
-    with open(file, "r", encoding=encoding) as f:
-        return csv.Sniffer().sniff(f.read(4096))
-
-
-def shuffle_csv(filepath: str, encoding: str) -> None:
-    with open(filepath, 'r', encoding=encoding) as fin:
+def shuffle_file(
+    filepath: str, encoding: str, shuffles: int, to_tmp: bool
+) -> list[str]:
+    files = []
+    with open(filepath, "r", encoding=encoding) as fin:
         rows = fin.readlines()
-    with open(f"{filepath}_shuff", "w", encoding=encoding) as fout:
-        fout.write(rows[0])
-        data = rows[1:]
-        shuffle(data)
-        fout.writelines(data)
-    
-    os.remove(filepath)
-    os.rename(f"{filepath}_shuff", filepath)
 
-    return filepath
+    filename = os.path.split(filepath)[1].split(".")[0]
+    out_path = os.path.join(TMP_DIR.name, filename)
+
+    headers = rows[0]
+    data = rows[1:]
+
+    for i in range(shuffles):
+        random.shuffle(data)
+
+        if shuffles > 1:
+            new_outpath = f"{out_path}_shuffle{i+1}"
+
+        with open(new_outpath, "w", encoding=encoding) as fout:
+            fout.write(headers)
+            fout.writelines(data)
+
+        files.append(new_outpath)
+
+    if not to_tmp:
+        os.remove(filepath)
+
+    return files
 
 
-def clean_filename(filename: str) -> str:
+def clean_filepath(filename: str) -> str:
     return re.sub(r"[^a-zA-Z0-9_]", "_", filename)
 
 
-def split_by_columns(filepath: str, col_list: list, rows: int, encoding: str, delimiter: str, output_directory: str, prefix: str, shuffle: bool) -> list[str]:
+def split_by_columns(
+    filepath: str, col_list: list, encoding: str, delimiter: str, to_tmp: bool
+) -> list[str]:
 
     with open(filepath, "r", encoding=encoding) as f:
         reader = csv.DictReader(f, delimiter=delimiter)
         files = {}
 
         for row in reader:
-            filepath = os.path.join(output_directory, prefix + clean_filename("_".join([row[col] for col in col_list])))
+            out_path = os.path.join(
+                TMP_DIR.name,
+                clean_filepath("_".join([row[col] for col in col_list])),
+            )
 
-            if filepath in files:
-                writer = files[filepath]["writer"]
+            if out_path in files:
+                writer = files[out_path]["writer"]
             else:
-                fout = open(filepath, "w", encoding=encoding)
-                writer = csv.DictWriter(fout, delimiter=delimiter, fieldnames=reader.fieldnames)
-                # writer = csv.DictWriter(fout, dialect=dialect, fieldnames=reader.fieldnames)
+                fout = open(out_path, "w", encoding=encoding, newline='')
+                writer = csv.DictWriter(
+                    fout, delimiter=delimiter, fieldnames=reader.fieldnames
+                )
                 writer.writeheader()
-                files.update({filepath: {"fout": fout, "writer": writer}})
+                files.update({out_path: {"fout": fout, "writer": writer}})
             
             writer.writerow(row)
-        
-        for file in files.values():
-            file["fout"].close()
-        
-        return list(files.keys())
+
+    for file in files.values():
+        file["fout"].close()
+
+    if not to_tmp:
+        os.remove(filepath)
+
+    return list(files.keys())
 
 
-def split_by_rows(filepath: str, encoding: str, rows: int) -> list[str]:
+def split_by_equal(filepath: str, encoding: str, equal: int, to_tmp: bool) -> list[str]:
+
+    with open(filepath, "r", encoding=encoding) as fin:
+        files = [os.path.join(TMP_DIR.name, f"{filepath}_{i+1}") for i in range(equal)]
+        fouts = [open(f, "w", encoding=encoding) for f in files]
+        fout_cycle = cycle(fouts)
+
+        for i, row in enumerate(fin):
+            if i == 0:
+                for fout in fouts:
+                    fout.write(row) # Write headers
+                continue
+
+            next(fout_cycle).write(row)
+
+    for fout in fouts:
+        fout.close()
+
+    if not to_tmp:
+        os.remove(filepath)
+
+    return files
+
+
+def split_by_rows(filepath: str, encoding: str, rows: int, to_tmp: bool) -> list[str]:
 
     with open(filepath, "r", encoding=encoding) as f:
-        headers = f.readline()
+        headers = ""
         filenum = 0
-        files = {}
+        files = []
+        fouts = []
 
         for i, row in enumerate(f):
-            if i % rows == 0:
+            if i == 0:
+                headers = row
+                continue
+
+            if (i - 1) % rows == 0:
                 filenum += 1
-                filename = f"{filepath}_{filenum}"
-                fout = open(filename, "w", encoding=encoding)
+                tmp_file = os.path.join(TMP_DIR.name, f"{filepath}_{filenum}")
+                files.append(tmp_file)
+                fout = open(tmp_file, "w", encoding=encoding)
+                fouts.append(fout)
                 fout.write(headers)
-                files.update({filename: fout})
 
             fout.write(row)
 
-        for file in files.values():
-            file.close()
-    
-    os.remove(filepath)
-    return list(files.keys())
-    
+    for fout in fouts:
+        fout.close()
+
+    if not to_tmp:
+        os.remove(filepath)
+
+    return files
+
 
 def main() -> None:
     kwargs = parse_args()
-    input_file = kwargs["input_file"]
-    output_directory = kwargs["output_directory"]
+    input_path = os.path.join(os.getcwd(), kwargs["input_path"])
+    output_directory = os.path.join(os.getcwd(), kwargs["output_directory"])
     columns = kwargs["columns"]
     rows = kwargs["rows"]
     encoding = kwargs["encoding"]
     prefix = kwargs["prefix"]
-    shuffle = kwargs["shuffle"]
+    shuffles = kwargs["shuffles"]
     delimiter = kwargs["delimiter"]
+    equal = kwargs["equal"]
+    to_tmp = True
 
-
-    files = [input_file]
-    os.makedirs(output_directory, exist_ok=True)
+    files = []
+    if os.path.isdir(input_path):
+        in_files = glob.glob(input_path + "/**/*.*", recursive=True)
+        combined_fp = os.path.join(TMP_DIR.name, "combined")
+        with open(combined_fp, "w", encoding=encoding) as fout:
+            for i, f in in_files:
+                with open(f, "r") as fin:
+                    if i > 0:
+                        next(fin)
+                    for line in fin:
+                        fout.write(line)
+        files.append(combined_fp)
+        to_tmp = False
+    else:
+        files.append(input_path)
 
     if columns:
         col_list = columns.split(",")
-        files = split_by_columns(input_file, col_list, rows, encoding, output_directory, prefix, shuffle)
-
-    if shuffle:
-        with Pool() as pool:
-            pool.starmap(shuffle_csv, zip(files, repeat(encoding)))
-
-    if rows > 0:
-        row_files = []
+        new_files = []
         for file in files:
-            row_files.extend(split_by_rows(file, encoding, rows))
+            new_files.extend(split_by_columns(file, col_list, encoding, delimiter, to_tmp))
 
-        files = row_files
+        files = new_files
+        to_tmp = False
 
+    if shuffles:
+        new_files = []
+        for file in files:
+            new_files.extend(shuffle_file(file, encoding, shuffles, to_tmp))
+
+        files = new_files
+        to_tmp = False
+
+    if rows:
+        new_files = []
+        for file in files:
+            new_files.extend(split_by_rows(file, encoding, rows, to_tmp))
+
+        files = new_files
+        to_tmp = False
+
+    if equal:
+        new_files = []
+        for file in files:
+            new_files.extend(split_by_equal(file, encoding, equal, to_tmp))
+
+        files = new_files
+        to_tmp = False
 
     for file in files:
-        if file.endswith(".csv"):
-            continue
-        os.rename(file, f"{file}.csv")
+        if prefix:
+            head, tail = os.path.split(file)
+            filename = f"{prefix}_{tail}"
+            newfile = os.path.join(head, filename)
+        os.rename(file, f"{newfile}.csv")
+
+    shutil.copytree(TMP_DIR.name, output_directory, dirs_exist_ok=True)
+    TMP_DIR.cleanup()
+
 
 if __name__ == "__main__":
     main()
